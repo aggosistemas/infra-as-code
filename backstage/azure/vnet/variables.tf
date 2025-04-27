7f01a103c601a950da7ba7fbb414ca3af6c6a879
Antonio Oliveira <aggosistemas@gmail.com>
2025-04-27 16:28:26 -0300

    correcao do arquivo variables

--- backstage/azure/vnet/variables.tf
+++ backstage/azure/vnet/variables.tf
@@ -1,6 +1,7 @@
 variable "resource_group_name" {
   description = "Nome do Resource Group a ser criado."
   type        = string
+  default     = "rg-pequistore"
 }
 
 variable "location" {
@@ -12,24 +13,29 @@ variable "location" {
 variable "vnet_name" {
   description = "Nome da Virtual Network (VNet) a ser criada."
   type        = string
+  default     = "vnet-pequistore"
 }
 
 variable "vnet_address_space" {
   description = "Faixa de IPs (CIDR) da VNet."
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "subnet_name" {
   description = "Nome da Subnet a ser criada dentro da VNet."
   type        = string
+  default     = "subnet-pequistore"
 }
 
 variable "subnet_address_prefix" {
   description = "Faixa de IPs (CIDR) da Subnet."
   type        = string
+  default     = "10.0.1.0/24"
 }
 
 variable "nsg_name" {
   description = "Nome do Network Security Group (NSG) a ser criado."
   type        = string
+  default     = "nsg-projeto-vm"
 }
