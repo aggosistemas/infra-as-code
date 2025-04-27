fe65b4d40efbde7f9b035f01e0a8d4bf70413384
Antonio Oliveira <aggosistemas@gmail.com>
2025-04-27 16:41:48 -0300

    revisão final do yaml

--- backstage/azure/vnet/variables.tf
+++ backstage/azure/vnet/variables.tf
@@ -39,3 +39,8 @@ variable "nsg_name" {
   type        = string
   default     = "nsg-projeto-vm"
 }
+
+variable "azure_client_id" {}
+variable "azure_client_secret" {}
+variable "azure_subscription_id" {}
+variable "azure_tenant_id" {}
