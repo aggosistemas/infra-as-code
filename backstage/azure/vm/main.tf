d4f3caa957a2a9dc40bc31683166751e565bc656
Antonio Oliveira <aggosistemas@gmail.com>
2025-05-04 20:44:11 -0300

    correção de network e aumento da pipe

--- backstage/azure/vm/main.tf
+++ backstage/azure/vm/main.tf
@@ -61,3 +61,33 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   depends_on = [azurerm_public_ip.public_ip]
 }
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg_name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-ICMP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Icmp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
