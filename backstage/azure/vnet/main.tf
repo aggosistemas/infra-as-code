cd86a088d483d5a7315c892ef25aaf68dff92c12
Antonio Oliveira <aggosistemas@gmail.com>
2025-05-04 20:57:46 -0300

    atualização arquivos de rede

--- backstage/azure/vnet/main.tf
+++ backstage/azure/vnet/main.tf
@@ -23,17 +23,29 @@ resource "azurerm_subnet" "subnet" {
 # Criar o Network Security Group (NSG)
 resource "azurerm_network_security_group" "nsg" {
   name                = var.nsg_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "SSH"
-    priority                   = 1001
+    name                       = "Allow-SSH"
+    priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-ICMP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Icmp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
