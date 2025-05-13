b6e9e047d67f1b573b70369792d4ef1af7086817
aggosistemas <125746491+aggosistemas@users.noreply.github.com>
2025-05-12 22:42:12 -0300

    Update main.tf

--- backstage/gcp/kubernetes/main.tf
+++ backstage/gcp/kubernetes/main.tf
@@ -6,6 +6,7 @@ provider "google" {
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
+  deletion_protection = false
 
   remove_default_node_pool = true
   initial_node_count       = 1
