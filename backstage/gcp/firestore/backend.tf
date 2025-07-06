df1e73dd0fed8b3fa242f7ee304647e6872e42d2
aggosistemas <125746491+aggosistemas@users.noreply.github.com>
2025-07-06 17:28:14 -0300

    Update backend.tf

--- backstage/gcp/firestore/backend.tf
+++ backstage/gcp/firestore/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-bucket-status"
-    key            = "terraform/state/firestore-gcp.tfstate"
+    key            = "terraform/state/firestore-gcp-pipeline.tfstate"
     region         = "ca-central-1"
     encrypt        = true
   }
