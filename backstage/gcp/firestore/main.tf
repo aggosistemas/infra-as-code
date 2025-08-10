255b0b4b3ac047f633745e522d69ca2731d00dd5
aggosistemas <aggosistemas@gmail.com>
2025-08-10 14:37:37 -0300

    Atualizacao do TTL

--- backstage/gcp/firestore/main.tf
+++ backstage/gcp/firestore/main.tf
@@ -13,3 +13,12 @@ resource "google_firestore_database" "default" {
   location_id = var.gcp_region
   type     = "FIRESTORE_NATIVE"
 }
+
+resource "google_firestore_field" "logs_pipeline_ttl" {
+  project    = var.gcp_project_id
+  database   = "agent-ia-active"
+  collection = "logs_pipeline"
+  field      = "ttl_hot"
+
+  ttl_config {}
+}
